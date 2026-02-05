d0ba6878c7b19c35ecd4c5f8bc31136c43a9aaa3
hellvita <seredaolga03@gmail.com>
2026-02-06 00:19:32 +0200

    add middleware for the unknown routes

--- src/server.js
+++ src/server.js
@@ -34,6 +34,12 @@ app.get('/notes/:noteId', (req, res) => {
   res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
 });
 
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route not found',
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
