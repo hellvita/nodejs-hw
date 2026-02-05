1a3c5c337dfaa71e3a448b64503a7bca0d763375
hellvita <seredaolga03@gmail.com>
2026-02-06 00:23:08 +0200

    add middleware to handle errors

--- src/server.js
+++ src/server.js
@@ -34,9 +34,19 @@ app.get('/notes/:noteId', (req, res) => {
   res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
 });
 
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
 app.use((req, res) => {
   res.status(404).json({
     message: 'Route not found',
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
   });
 });
 
