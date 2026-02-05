f9a52784000a021e6d65ef75aa62869a07015746
hellvita <seredaolga03@gmail.com>
2026-02-06 00:16:40 +0200

    add the notes/id route

--- src/server.js
+++ src/server.js
@@ -29,6 +29,11 @@ app.get('/notes', (req, res) => {
   res.status(200).json({ message: 'Retrieved all notes' });
 });
 
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
