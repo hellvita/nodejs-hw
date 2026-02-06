25acebdf0f97d6b63b7bb11540dd92191643dd3c
hellvita <seredaolga03@gmail.com>
2026-02-07 01:43:41 +0200

    add the connectMongoDB method

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import 'dotenv/config';
+import { connectMongoDB } from './db/connectMongoDB.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -49,6 +50,8 @@ app.use((err, req, res, next) => {
     message: err.message,
   });
 });
+
+await connectMongoDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
