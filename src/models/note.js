4dcf5818df95639db4461c07ba450c8a5a4b9f44
hellvita <seredaolga03@gmail.com>
2026-02-07 01:49:06 +0200

    add the note schema

---
+++ src/models/note.js
@@ -0,0 +1,29 @@
+import { Schema } from 'mongoose';
+
+const noteSchema = new Schema(
+  {
+    title: { type: String, required: true, trim: true },
+    content: { type: String, required: false, default: '' },
+    tag: {
+      type: String,
+      required: false,
+      enum: [
+        'Work',
+        'Personal',
+        'Meeting',
+        'Shopping',
+        'Ideas',
+        'Travel',
+        'Finance',
+        'Health',
+        'Important',
+        'Todo',
+      ],
+      default: 'Todo',
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
