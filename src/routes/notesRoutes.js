ac9c0ed76e1eb0a592acb976f6f374c6f118a477
hellvita <seredaolga03@gmail.com>
2026-02-07 03:00:00 +0200

    change the export type

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,16 +1,22 @@
 import { Router } from 'express';
-import * as controller from '../controllers/notesController.js';
+import {
+  getAllNotes,
+  getNoteById,
+  createNote,
+  deleteNote,
+  updateNote,
+} from '../controllers/notesController.js';
 
 const router = Router();
 
-router.get('/notes', controller.getAllNotes);
+router.get('/notes', getAllNotes);
 
-router.get('/notes/:noteId', controller.getNoteById);
+router.get('/notes/:noteId', getNoteById);
 
-router.post('/notes', controller.createNote);
+router.post('/notes', createNote);
 
-router.delete('/notes/:noteId', controller.deleteNote);
+router.delete('/notes/:noteId', deleteNote);
 
-router.patch('/notes/:noteId', controller.updateNote);
+router.patch('/notes/:noteId', updateNote);
 
 export default router;
