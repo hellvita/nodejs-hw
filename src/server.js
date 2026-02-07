c24c61d1ff5d45438722ccc7f4acb9c1911ae7f6
hellvita <seredaolga03@gmail.com>
2026-02-07 02:55:28 +0200

    update the variable name

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
-import noteRoutes from './routes/notesRoutes.js';
+import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -14,7 +14,7 @@ app.use(logger);
 app.use(express.json());
 app.use(cors());
 
-app.use(noteRoutes);
+app.use(notesRoutes);
 
 app.use(notFoundHandler);
 
