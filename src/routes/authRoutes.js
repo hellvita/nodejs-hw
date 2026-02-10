1da1389ebd985cfe06720da2f85d8b92a36458d0
hellvita <seredaolga03@gmail.com>
2026-02-10 17:51:10 +0200

    add the refresh and logout routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,11 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
-import { registerUser, loginUser } from '../controllers/authController.js';
+import {
+  registerUser,
+  loginUser,
+  refreshUserSession,
+  logoutUser,
+} from '../controllers/authController.js';
 import {
   registerUserSchema,
   loginUserSchema,
@@ -10,5 +15,7 @@ const router = Router();
 
 router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
+router.post('/auth/refresh', refreshUserSession);
+router.post('/auth/logout', logoutUser);
 
 export default router;
