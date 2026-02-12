763576a38fc4a43183048ce374f18a02ef27c2f2
hellvita <seredaolga03@gmail.com>
2026-02-12 21:45:23 +0200

    add the userRoutes to the server

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import { errors } from 'celebrate';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -20,6 +21,7 @@ app.use(cookieParser());
 
 app.use(authRoutes);
 app.use(notesRoutes);
+app.use(userRoutes);
 
 app.use(notFoundHandler);
 app.use(errors());
