736d3eaa1df9a96625afc94d2b7f8cf30dc80761
hellvita <seredaolga03@gmail.com>
2026-02-12 21:59:25 +0200

    update the requestResetEmail controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -113,7 +113,7 @@ export const requestResetEmail = async (req, res) => {
   const template = handlebars.compile(templateSource);
   const html = template({
     name: user.username,
-    link: `${process.env.FRONTEND_DOMAIN}/auth/reset-password?token=${resetToken}`,
+    link: `${process.env.FRONTEND_DOMAIN}/reset-password?token=${resetToken}`,
   });
 
   try {
