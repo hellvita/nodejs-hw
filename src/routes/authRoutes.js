906771b6691b698be3e0869fee09d630a4cde42b
hellvita <seredaolga03@gmail.com>
2026-02-12 21:00:08 +0200

    add the route to send reset email

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,10 +5,12 @@ import {
   loginUser,
   refreshUserSession,
   logoutUser,
+  requestResetEmail,
 } from '../controllers/authController.js';
 import {
   registerUserSchema,
   loginUserSchema,
+  requestResetEmailSchema,
 } from '../validations/authValidation.js';
 
 const router = Router();
@@ -20,5 +22,11 @@ router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/auth/refresh', refreshUserSession);
 
 router.post('/auth/logout', logoutUser);
+
+router.post(
+  '/auth/request-reset-email',
+  celebrate(requestResetEmailSchema),
+  requestResetEmail,
+);
 
 export default router;
