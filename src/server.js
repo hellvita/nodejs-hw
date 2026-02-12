bb3bcdc102b51e76ce99081d54af9fe532083761
hellvita <seredaolga03@gmail.com>
2026-02-12 21:46:34 +0200

    fix the path to the userRoutes

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ import { errors } from 'celebrate';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
-import userRoutes from './routes/userRoutes';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
